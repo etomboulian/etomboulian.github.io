8bd5e60d7b7775133e0c3aec2efec1c1f6b9e8e9
Evan Tomboulian <etomboulian@gmail.com>
2020-06-13 12:50:40 -0700

    Revert "Revert "redo personal github site""
    This reverts commit 8bd5a96301a3141b6e75ef26ec8c5d94c11174ac.

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="One Page Website/Portfolio Template Which Is Too Minimal." />
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://fr0st1n.github.io/TooMinimal/">
+    <meta property="og:title" content="TooMinimal">
+    <meta property="og:description" content="One Page Website/Portfolio Template Which Is Too Minimal.">
+    <meta property="og:image" content="https://fr0st1n.github.io/TooMinimal/images/thumb.jpg">
+    <title>Evan's Minimal Home Page</title>
+    <link rel="shortcut icon" type="image/png" href="images/favicon.png" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/font.css">
+    <link rel="stylesheet" href="css/style.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1 class="username">Evan Tomboulian</h1>
+        </div>
+        <hr class="line">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>About me</h2>
+                <p>Under Construction</p>
+                <br />
+            </div>
+            <div class="col-md-6">
+                <h2>Projects</h2>
+                <!-- comment out default projects; use as template for new project                
+				<ul>
+                    <li><a href="https://github.com/tensorflow/tensorflow" target="_blank" rel=”noopener noreferrer”>tensorflow</a></li>
+                </ul>
+                -->
+                <ul> 
+                    <li><a href="http://evan.tomboulian.ca" target="_self">This resume page!</li>                                       
+                    <li><a href="projects/map.php" target="_blank">Server Map</a></li>
+                    <li><a href="http://tomboulian.ca/calculator">Javascript Calculator</a></li> 
+                    <li><a href="https://github.com/etomboulian/plusbot" target="_blank">Plusbot Slack Addon</a> [rebuild functionality of plusplus]</li>
+                </ul>
+                <br>
+            </div>
+
+            <div class="col-12">
+                <h2>Professional Experience</h2>
+                <div>
+                    <div class="table table-row first-row">
+                        <div class="item"><img src="images/active.jpg" class="thumbnail" /></div>
+                        <div class="item">Active Network</div>
+                        <div class="item">Technical Analyst, Senior</div>
+                        <div class="item">2019 - Now</div>
+                        <i class="arrow item fas fa-angle-down" id="PE-TA-Senior" onclick="insertDetailRow(this);"></i>
+                    </div>
+
+                    <div class="table table-row">
+                        <div class="item"><img src="images/active.jpg" class="thumbnail"></div>
+                        <div class="item">Active Network</div>
+                        <div class="item">Technical Analyst</div>
+                        <div class="item">2014 - 2019</div>
+                        <i class="arrow item fas fa-angle-down" id="PE-TA" onclick="insertDetailRow(this);"></i>
+                    </div>
+                    <div class="table table-row">
+                        <div class="item"><img src="images/active.jpg" class="thumbnail" /></div>
+                        <div class="item">Active Network</div>
+                        <div class="item">Client Application Specialist</div>
+                        <div class="item">2013 - 2014</div>
+                        <i class="arrow item fas fa-angle-down" id="PE-CAS" onclick="insertDetailRow(this);"></i>
+                    </div>
+                    <div class="table table-row">
+                        <div class="item"><img src="images/w-logo.png" class="thumbnail" /></div>
+                        <div class="item">Windsor Plywood</div>
+                        <div class="item">Sales/Customer Service</div>
+                        <div class="item">2008 - 2013</div>
+                        <i class="arrow item fas fa-angle-down" id="PE-WPly" onclick="insertDetailRow(this);"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <h2>Education</h2>
+                <div class="" id="edu-table">
+                    
+                    <div class="table table-row first-row">
+                        <div class="item"><img src="images/bcit-logo.png" class="thumbnail"></div>
+                        <div class="item">British Columbia Institute of Technology</div>
+                        <div class="item">Applied Software Development Associate Certificate</div>
+                        <div class="item">2021 (est)</div>
+                        <i class="arrow item fas fa-angle-down" id="ED-BCIT-ASD" onclick="insertDetailRow(this);"></i>
+                    </div>
+                    <div class="table table-row">
+                        <div class="item"><img src="images/bcit-logo.png" class="thumbnail"></div>
+                        <div class="item">British Columbia Institute of Technology</div>
+                        <div class="item">Applied Network Administration and Design Associate Certificate</div>
+                        <div class="item">2021 (est)</div>
+                        <i class="arrow item fas fa-angle-down" id="ED-BCIT-ANAD" onclick="insertDetailRow(this);"></i>
+                    </div>
+                    <div class="table table-row">
+                        <div class="item"><img src="images/SFU-block-logo.svg" class="thumbnail"></div>
+                        <div class="item">Simon Fraser University</div>
+                        <div class="item">Bachelor of Science</div>
+                        <div class="item">2012</div>
+                        <i class="arrow item fas fa-angle-down" id="ED-SFU" onclick="insertDetailRow(this);"></i>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <h2>Certifications</h2>
+
+                <div class="" id="cert-table">
+                    <div class="table table-row first-row">
+                        <div class="item"><img src="images/network-plus-logo.svg" class="thumbnail"> </div>
+                        <div class="item">CompTIA Network+</div>
+                        <div class="item middle text-warning">In Progress</div>
+                        <div class="item">2020 (est)</div>
+                        <i class="arrow item fas fa-angle-down" id="CERT-Net+" onclick="insertDetailRow(this);"></i>
+                    </div>
+
+<!-- Commenting out certs that are on my list for future consideration 
+
+                    <div class="table table-row disabled">
+                        <div class="item"><img src="images/logosecurityplus.svg" class="thumbnail"></div>
+                        <div class="item">CompTIA Security+</div>
+                        <div class="item middle text-warning">In Progress</div>
+                        <div class="item">2021 (est)</div>
+                        <i class="arrow item fas fa-angle-down" onclick="insertDetailRow(this);"></i>
+                    </div>
+                    <div class="table table-row disabled">
+                        <div class="item"><img src="images/oscp-logo.png" class="thumbnail"></div>
+                        <div class="item">Offensive Security Ceritified Professional</div>
+                        <div class="item middle text-warning">In Progress</div>
+                        <div class="item">2022 (est)</div>
+                        <i class="arrow item fas fa-angle-down" onclick="insertDetailRow(this);"></i>
+                    </div>
+	End cert comment for certs not achieved and under consideration	-->
+                </div>
+            </div>
+		    
+            <div class="col-md-12">
+		<hr class="line">
+                <h2>Contact</h2>
+                <p>
+                    <i class="fas fa-envelope"></i>
+                    <strong>Email: </strong>&nbsp&nbsp<a href="mailto:etomboulian@gmail.com">etomboulian@gmail.com</a>
+                    <br>
+                    <i class="fab fa-twitter-square"></i>
+                    <strong>Twitter: </strong>&nbsp<a href="https://twitter.com/etomboulian">@etomboulian</a>
+                    <br>
+                    <i class="fab fa-github-square"></i>
+                    <strong>Github: </strong>&nbsp<a href="https://github.com/etomboulian">etomboulian</a>
+                </p>
+
+            </div>
+        </div>
+    </div>
+    <script src="js/custom.js"> </script>
+</body>
+
+</html>
